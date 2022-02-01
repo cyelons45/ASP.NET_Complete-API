15f94b3442529156345d6b781e8be376d5b31941
samuel nyarkoh <52537459+cyelons45@users.noreply.github.com>
2022-02-01 08:07:29 -0500

    Initial commit
    Initial commit

---
+++ Sibly/App_Start/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Sibly.Dtos;
+using Sibly.Models;
+
+namespace Sibly.App_Start
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>().ReverseMap().ForMember(c => c.CustomerId, opt => opt.Ignore());
+            Mapper.CreateMap<CustomerDto, Customer>();
+
+
+            Mapper.CreateMap<Movie, MoviesDto>().ReverseMap().ForMember(m => m.MovieId, opt => opt.Ignore());
+            Mapper.CreateMap<Movie, MoviesDto>();
+            
+        }
+    }
+}
